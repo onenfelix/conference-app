fa25b77d5afc0b4f66b657db866714e328211d85
Felix <onenfelix90@gmail.com>
2023-10-01 11:37:19 +0300

    Added manytomany relationship between speakers and sessions

--- src/main/java/com/swinpostnetworks/conferencedemo/models/Speaker.java
+++ src/main/java/com/swinpostnetworks/conferencedemo/models/Speaker.java
@@ -1,9 +1,9 @@
 package com.swinpostnetworks.conferencedemo.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.util.List;
 
 @Entity(name = "speakers")
 public class Speaker {
@@ -15,6 +15,30 @@ public class Speaker {
     private String title;
     private String company;
     private String speaker_bio;
+
+    @Lob
+    @Type(type = "org.hibernate.type.BinaryType")
+    private byte[] speaker_photo;
+
+    @ManyToMany(mappedBy = "speakers")
+    private List<Session> sessions;
+
+    public List<Session> getSessions() {
+        return sessions;
+    }
+
+    public byte[] getSpeaker_photo() {
+        return speaker_photo;
+    }
+
+    public void setSpeaker_photo(byte[] speaker_photo) {
+        this.speaker_photo = speaker_photo;
+    }
+
+    public void setSessions(List<Session> sessions) {
+        this.sessions = sessions;
+    }
+
     public Speaker() {
     }
 
